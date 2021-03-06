f866d3a60f8062d91a9f12ad5ac898bf625fa311
Brandon <brandonbgrayson@gmail.com>
2021-03-06 16:09:16 -0500

    css title element global styles imported

--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,7 @@ import Header from './components/header'
 export default function Home() {
   return (
     <>
-    <Header/>
-    <h1>Welcome to Mastering Next.js</h1>
+    <p className='title'>CSS</p>
     </>
   )
 }
