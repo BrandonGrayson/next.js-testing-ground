357dce314c5d7f8111a49ced6578f4e030e49640
Brandon <brandonbgrayson@gmail.com>
2021-03-05 20:11:23 -0500

    getInitialProps func now makes async api call to display json data

--- pages/post/[id]/index.js
+++ pages/post/[id]/index.js
@@ -1,17 +1,19 @@
-import {useRouter} from "next/router";
+
 import Header from "../../components/header"
 
-const Post = () => {
-    const router = useRouter();
-    const {id} = router.query
-
-    return (
+const Post = (title, body) => (
         <>
-        <Header/>
-    <h1>Post: {id}</h1>
+            <Header />
+            <h1>{title}</h1>
+            <p>{body}</p>
         </>
     )
-}
 
+
+Post.getInitialProps = async ({ props }) => {
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${query.id}`)
+    const post = res.json()
+    return
+}
 export default Post
 
