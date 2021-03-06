b5b7ade258dbef45a21b5fc89404bbff507eb39e
Brandon <brandonbgrayson@gmail.com>
2021-03-05 20:58:14 -0500

    added Login page

---
+++ pages/login.js
@@ -0,0 +1,18 @@
+import {useRouter}  from "next/router"
+const Login = () => {
+    const router = useRouter();
+    const onLogin = (event) => {
+        event.preventDefault();
+
+        router.push('./dashboard')
+    };
+    return (
+        <>
+        <input placeholder='UserName' />
+        <input placeholder='Password' type='password' />
+        <button onClick={onLogin} type='button'>LogIn</button>
+        </>
+    );
+};
+
+export default Login
